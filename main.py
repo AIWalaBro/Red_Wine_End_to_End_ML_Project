c4feb635e146ca85effccf8495be8dbdeb1796d9
AIWalaBro <aiwalabro@gmail.com>
2024-01-12 22:55:11 +0530

    data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from redwine_ml import logger
 from redwine_ml.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from redwine_ml.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from redwine_ml.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -25,3 +27,17 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Data transformation Stage"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_transformation = DataTransformationTrainingPipeline()
+        data_transformation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
